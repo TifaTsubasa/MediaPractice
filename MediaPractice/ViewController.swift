55a7e3a288a81a13b34ef49451e9ed9142295a4d
upmer <upmer@upmer.com>
2020-01-15 15:45:18 +0800

    Initial Commit

---
+++ MediaPractice/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MediaPractice
+//
+//  Created by upmer on 2020/1/15.
+//  Copyright © 2020 upmer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
